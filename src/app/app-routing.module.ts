ada71a216449e4062d2a23c0a6787757c7e11781
Thiago Silva <thiagorodriguesdz7@gmail.com>
2021-10-27 15:13:03 -0300

    ProjetoDuplaAngularCast

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './contatos/contatos.component';
+import { JogosComponent } from './jogos/jogos.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'jogos', component:JogosComponent},
+  {path:'contatos', component:ContatosComponent},
+  {path:'sobreNos', component:SobreNosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JogosComponent } from './jogos/jogos.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JogosComponent,
+    SobreNosComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule,
